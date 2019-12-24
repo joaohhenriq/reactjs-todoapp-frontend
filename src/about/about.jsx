e831621c8c7e7b412b0cacaaf24c1dc8c11697af
joaoh <joao_camargo10@hotmail.com>
2019-12-24 16:42:04 -0300

    Page header

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>About</h1>
+        <PageHeader name='About' small='Us'></PageHeader>
+
+        <h2>Our history</h2>
+        <p>Lorem lorem lorem...</p>
+        <h2>Mission and focus</h2>
+        <p>Ipsum ipsum ipsum...</p>
+        <h2>Others</h2>
+        <p>Dolor dolor dolor...</p>
     </div>
 )
