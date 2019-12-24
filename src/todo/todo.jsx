7c402d7e820424a4f78986a5b7df76a6c02c1b33
joaoh <joao_camargo10@hotmail.com>
2019-12-24 19:15:11 -0300

    Button and input handle (Form)

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,11 +4,32 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
+
+    handleChange(e) { //event -> has the target(input), and input has the value
+        this.setState({
+            ...this.state, description: e.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tasks' small='Register'></PageHeader>
-                <TodoForm ></TodoForm>
+                <TodoForm description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                ></TodoForm>
                 <TodoList ></TodoList>
             </div>
         )
